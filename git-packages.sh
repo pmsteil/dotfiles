76c01fc2dead6b2b44b9f7ad394d943862549755
Patrick Steil <patrick@infranet.com>
2019-12-17 22:50:16 -0600

    add new files

---
+++ git-packages.sh
@@ -0,0 +1,13 @@
+
+
+# setup my local git stuff
+mkdir -p ~/git
+cd ~/git
+
+git clone git@gitlab.com:wd-magento-group/wd-qatools.git
+git clone git@gitlab.com:wd-magento-group/wd-mag-localdev.git
+git clone git@gitlab.com:wd-magento-group/wdmag-deploy.git
+git $ git clone git@github.com:pmsteil/Magento-Cloud.git github-magento-cloud-fork
+      cd github-magento-cloud-fork
+      git remote add wdirect git@github.com:pmsteil/Magento-Cloud.git
+      cd ..
---
+++ macos-applications.sh
@@ -0,0 +1,8 @@
+# Applications.sh
+
+#install docker desktop instead
+read -p "Press the any key to download and install Docker Desktop"
+open https://download.docker.com/mac/stable/Docker.dmg
+
+read -p "Press the any key to download and install screencast-o-matic"
+open https://screencast-o-matic.com/account
