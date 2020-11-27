a23dd6b48f65573779712e0a885c83e03cade1d1
Patrick Steil <patrick@infranet.com>
2020-11-27 09:17:32 -0600

    tweak

--- installShell.sh
+++ installShell.sh
@@ -3,12 +3,16 @@
 clear
 pushd ~/git/dotFiles > /dev/null
 
-git status
+if [ -n "$(git status --porcelain)" ]; then
+    git status
+    echo ""
+    echo ""
+    echo "---------------------------------------------------------------------"
+    read -p "Make sure there are no outstanding commits in ~/git/dotFiles and press ENTER..."
+else
+  echo "no changes";
+fi
 
-echo ""
-echo ""
-echo "---------------------------------------------------------------------"
-read -p "Make sure there are no outstanding commits and press ENTER..."
 
 echo Installing my personal bin folder
 ./installbins.sh
@@ -25,5 +29,6 @@ source ~/.bash_profile
 echo You must run the following command to get all the latest shell features
 echo "  reload"
 echo ""
+
 
 popd > /dev/null
